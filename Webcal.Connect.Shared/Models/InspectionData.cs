cd7459ee99af9a17d2c0108ee7254114345f6f06
Jon Preece <jonpreece@hotmail.co.uk>
2016-01-09 08:32:53 +0000

    Added inspection data

---
+++ Webcal.Connect.Shared/Models/InspectionData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Connect.Shared.Models
+{
+    public class InspectionData
+    {
+        public string Message { get; set; }
+        public DateTime Entered { get; set; }
+    }
+}
