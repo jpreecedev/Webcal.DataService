da6b8eb930b9d3450de48489e7881744e8373189
Jon Preece <jonpreece@hotmail.co.uk>
2016-03-19 07:49:15 +0000

    Added GV 212 data object

---
+++ Webcal.Connect.Shared/Models/GV212Report.cs
@@ -0,0 +1,36 @@
+﻿namespace Connect.Shared.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class GV212Report : BaseModel, IEquatable<GV212Report>
+    {
+        public DateTime Created { get; set; }
+
+        [MaxLength]
+        public byte[] SerializedData { get; set; }
+
+        public bool Equals(GV212Report other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Created.Equals(other.Created) && Equals(SerializedData, other.SerializedData);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((GV212Report) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Created.GetHashCode()*397) ^ (SerializedData != null ? SerializedData.GetHashCode() : 0);
+            }
+        }
+    }
+}
