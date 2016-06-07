6b2aca5ca3330dd60be1b0c0e1c75331f70844cb
Jon Preece <jonpreece@hotmail.co.uk>
2016-06-07 03:54:41 +0100

    Added FastQueryHelper

---
+++ Webcal.Connect.Shared/FastQueryHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Connect.Shared
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using Models;
+
+    public class FastQueryHelper
+    {
+        public static string GetSqlQueryFor<T>(bool includeSchema) where T : Document
+        {
+            var properties = typeof(T).GetProperties().Where(c => !Attribute.IsDefined(c, typeof(NotMappedAttribute)))
+                .Where(c => c.Name != "SerializedData")
+                .Where(c => c.GetSetMethod() != null);
+
+            var builder = new StringBuilder();
+            builder.Append("SELECT ");
+
+            foreach (var propertyInfo in properties)
+            {
+                builder.Append(propertyInfo.Name + ", ");
+            }
+
+            builder.Append("NULL AS SerializedData");
+
+            var result = builder.ToString();
+            result = result + " FROM " + (includeSchema ? "dbo." : "") + "[" + typeof(T).Name + "s]";
+
+            return result;
+        }
+    }
+}
